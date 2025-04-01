32fc4fb12d8ebfd9882c25ab73ee1ee8c228d1a6
Dev Talan <chaudhary.dev.talan@gmail.com>
2025-04-01 18:49:08 +0000

    fix: fixed breadcrumbs

--- src/app/(authenticated)/dashboard/account/create-organization/page.tsx
+++ src/app/(authenticated)/dashboard/account/create-organization/page.tsx
@@ -1,48 +1,13 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@components/ui/breadcrumb";
-import { Separator } from "@components/ui/separator";
-import { SidebarInset, SidebarTrigger } from "@components/ui/sidebar";
 import CreateOrganizationForm from "@features/user/components/create-organization-form";
 
 const CreateOrganizationPage = () => {
   return (
     <div>
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="/dashboard/account">
-                    Account
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Create New Organization</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div>
-            <CreateOrganizationForm />
-          </div>
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div>
+          <CreateOrganizationForm />
         </div>
-      </SidebarInset>
+      </div>
     </div>
   );
 };
--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -1,14 +1,6 @@
 "use client";
 
 // import { headers } from "next/headers";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbList,
-  BreadcrumbPage,
-} from "@components/ui/breadcrumb";
-import { Separator } from "@components/ui/separator";
-import { SidebarInset, SidebarTrigger } from "@components/ui/sidebar";
 
 // import { auth } from "@/auth/auth";
 
@@ -22,25 +14,10 @@ const DashboardPage = () => {
 
   return (
     <div>
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Dashboard</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          {/*<div>{JSON.stringify(user)}</div>*/}
-          {/*<div>{JSON.stringify(activeOrganization)}</div>*/}
-        </div>
-      </SidebarInset>
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        {/*<div>{JSON.stringify(user)}</div>*/}
+        {/*<div>{JSON.stringify(activeOrganization)}</div>*/}
+      </div>
     </div>
   );
 };
